24247b5f3553153d6c8e50e398fcbe3004c87eba
xixilsun <xixilliasun@gmail.com>
2020-10-24 05:56:55 +0700

    Initial commit

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,141 @@
+<!doctype html>
+<html lang="en">
+   <head>
+      <!-- Required meta tags -->
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+      <title>Login</title>
+      <!-- Favicon -->
+      <link rel="shortcut icon" href="{{asset('images/favicon.ico')}}" />
+      <!-- Bootstrap CSS -->
+      <link rel="stylesheet" href="{{asset('template/css/bootstrap.min.css')}}">
+      <!-- Typography CSS -->
+      <link rel="stylesheet" href="{{asset('template/css/typography.css')}}">
+      <!-- Style CSS -->
+      <link rel="stylesheet" href="{{asset('template/css/style.css')}}">
+      <!-- Responsive CSS -->
+      <link rel="stylesheet" href="{{asset('template/css/responsive.css')}}">
+   </head>
+   <body>
+      <!-- loader Start -->
+      <div id="loading">
+         <div id="loading-center">
+         </div>
+      </div>
+      <!-- loader END -->
+        <!-- Sign in Start -->
+        <section class="sign-in-page">
+          <div id="container-inside">
+              <div id="circle-small"></div>
+              <div id="circle-medium"></div>
+              <div id="circle-large"></div>
+              <div id="circle-xlarge"></div>
+              <div id="circle-xxlarge"></div>
+          </div>
+            <div class="container p-0">
+                <div class="row no-gutters">
+                    <div class="col-md-6 text-center pt-5">
+                        <div class="sign-in-detail text-white">
+                            <a class="sign-in-logo mb-5" href="#"><img src="{{asset('template/images/logo-full.png')}}" class="img-fluid" alt="logo"></a>
+                            <div class="owl-carousel" data-autoplay="true" data-loop="true" data-nav="false" data-dots="true" data-items="1" data-items-laptop="1" data-items-tab="1" data-items-mobile="1" data-items-mobile-sm="1" data-margin="0">
+                                <div class="item">
+                                    <img src="{{asset('template/images/login/1.png')}}" class="img-fluid mb-4" alt="logo">
+                                    <h4 class="mb-1 text-white">Find new friends</h4>
+                                    <p>It is a long established fact that a reader will be distracted by the readable content.</p>
+                                </div>
+                                <div class="item">
+                                    <img src="{{asset('template/images/login/2.png')}}" class="img-fluid mb-4" alt="logo"> 
+                                    <h4 class="mb-1 text-white">Connect with the world</h4>
+                                    <p>It is a long established fact that a reader will be distracted by the readable content.</p>
+                                </div>
+                                <div class="item">
+                                    <img src="{{asset('template/images/login/3.png')}}" class="img-fluid mb-4" alt="logo">
+                                    <h4 class="mb-1 text-white">Create new events</h4>
+                                    <p>It is a long established fact that a reader will be distracted by the readable content.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-6 bg-white pt-5">
+                        <div class="sign-in-from">
+                            <h1 class="mb-0">Sign in</h1>
+                            <p>Enter your email address and password to access admin panel.</p>
+                            <form class="mt-4" method="POST" action="{{ route('login') }}">
+                                @csrf
+                                <div class="form-group">
+                                    <label for="email">Email address</label>
+                                    <input type="email" id="email" class="form-control mb-0 @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                                    @error('email')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+                                </div>
+                                <div class="form-group">
+                                    <label for="password">Password</label>
+                                    <input type="password" class="form-control mb-0 @error('password') is-invalid @enderror" name="password" required autocomplete="current-password" id="password">
+
+                                    @error('password')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+                                </div>
+                                <div class="d-inline-block w-100">
+                                    <div class="custom-control custom-checkbox d-inline-block mt-2 pt-1">
+                                        <!-- <input type="checkbox" class="custom-control-input" id="customCheck1"> -->
+                                        <input class="form-check-input custom-control-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+
+                                        <label class="custom-control-label" for="remember">Remember Me</label>
+                                    </div>
+                                    <button type="submit" class="btn btn-primary float-right">Sign in</button>
+                                </div>
+                                <div class="sign-info">
+                                    <span class="dark-color d-inline-block line-height-2">Don't have an account? <a href="/register">Sign up</a></span>
+                                    <ul class="iq-social-media">
+                                        <li><a href="#"><i class="ri-facebook-box-line"></i></a></li>
+                                        <li><a href="#"><i class="ri-twitter-line"></i></a></li>
+                                        <li><a href="#"><i class="ri-instagram-line"></i></a></li>
+                                    </ul>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- Sign in END -->
+      <!-- Optional JavaScript -->
+      <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+      <script src="{{asset('template/js/jquery.min.js')}}"></script>
+      <script src="{{asset('template/js/popper.min.js')}}"></script>
+      <script src="{{asset('template/js/bootstrap.min.js')}}"></script>
+      <!-- Appear JavaScript -->
+      <script src="{{asset('template/js/jquery.appear.js')}}"></script>
+      <!-- Countdown JavaScript -->
+      <script src="{{asset('template/js/countdown.min.js')}}"></script>
+      <!-- Counterup JavaScript -->
+      <script src="{{asset('template/js/waypoints.min.js')}}"></script>
+      <script src="{{asset('template/js/jquery.counterup.min.js')}}"></script>
+      <!-- Wow JavaScript -->
+      <script src="{{asset('template/js/wow.min.js')}}"></script>
+      <!-- Apexcharts JavaScript -->
+      <script src="{{asset('template/js/apexcharts.js')}}"></script>
+      <!-- lottie JavaScript -->
+      <script src="{{asset('template/js/lottie.js')}}"></script>
+      <!-- Slick JavaScript --> 
+      <script src="{{asset('template/js/slick.min.js')}}"></script>
+      <!-- Select2 JavaScript -->
+      <script src="{{asset('template/js/select2.min.js')}}"></script>
+      <!-- Owl Carousel JavaScript -->
+      <script src="{{asset('template/js/owl.carousel.min.js')}}"></script>
+      <!-- Magnific Popup JavaScript -->
+      <script src="{{asset('template/js/jquery.magnific-popup.min.js')}}"></script>
+      <!-- Smooth Scrollbar JavaScript -->
+      <script src="{{asset('template/js/smooth-scrollbar.js')}}"></script>
+      <!-- Chart Custom JavaScript -->
+      <script src="{{asset('template/js/chart-custom.js')}}"></script>
+      <!-- Custom JavaScript -->
+      <script src="{{asset('template/js/custom.js')}}"></script>
+   </body>
+</html>
---
+++ resources/views/template/partials/events.blade.php
@@ -0,0 +1,153 @@
+<div class="col-lg-4">
+   <div class="iq-card">
+      <div class="iq-card-header d-flex justify-content-between">
+         <div class="iq-header-title">
+            <h4 class="card-title">Stories</h4>
+         </div>
+      </div>
+      <div class="iq-card-body">
+         <ul class="media-story m-0 p-0">
+            <li class="d-flex mb-4 align-items-center">
+               <i class="ri-add-line font-size-18"></i>
+               <div class="stories-data ml-3">
+                  <h5>Creat Your Story</h5>
+                  <p class="mb-0">time to story</p>
+               </div>
+            </li>
+            <li class="d-flex mb-4 align-items-center active">
+               <img src="{{asset('template/images/page-img/s2.jpg')}}" alt="story-img" class="rounded-circle img-fluid">
+               <div class="stories-data ml-3">
+                  <h5>Anna Mull</h5>
+                  <p class="mb-0">1 hour ago</p>
+               </div>
+            </li>
+            <li class="d-flex mb-4 align-items-center">
+               <img src="{{asset('template/images/page-img/s3.jpg')}}" alt="story-img" class="rounded-circle img-fluid">
+               <div class="stories-data ml-3">
+                  <h5>Ira Membrit</h5>
+                  <p class="mb-0">4 hour ago</p>
+               </div>
+            </li>
+            <li class="d-flex align-items-center">
+               <img src="{{asset('template/images/page-img/s1.jpg')}}" alt="story-img" class="rounded-circle img-fluid">
+               <div class="stories-data ml-3">
+                  <h5>Bob Frapples</h5>
+                  <p class="mb-0">9 hour ago</p>
+               </div>
+            </li>
+         </ul>
+         <a href="#" class="btn btn-primary d-block mt-3">See All</a>
+      </div>
+   </div>
+   <div class="iq-card">
+      <div class="iq-card-header d-flex justify-content-between">
+         <div class="iq-header-title">
+            <h4 class="card-title">Events</h4>
+         </div>
+         <div class="iq-card-header-toolbar d-flex align-items-center">
+            <div class="dropdown">
+               <span class="dropdown-toggle" id="dropdownMenuButton" data-toggle="dropdown" aria-expanded="false" role="button">
+               <i class="ri-more-fill"></i>
+               </span>
+               <div class="dropdown-menu dropdown-menu-right" aria-labelledby="dropdownMenuButton" style="">
+                  <a class="dropdown-item" href="#"><i class="ri-eye-fill mr-2"></i>View</a>
+                  <a class="dropdown-item" href="#"><i class="ri-delete-bin-6-fill mr-2"></i>Delete</a>
+                  <a class="dropdown-item" href="#"><i class="ri-pencil-fill mr-2"></i>Edit</a>
+                  <a class="dropdown-item" href="#"><i class="ri-printer-fill mr-2"></i>Print</a>
+                  <a class="dropdown-item" href="#"><i class="ri-file-download-fill mr-2"></i>Download</a>
+               </div>
+            </div>
+         </div>
+      </div>
+      <div class="iq-card-body">
+         <ul class="media-story m-0 p-0">
+            <li class="d-flex mb-4 align-items-center ">
+               <img src="{{asset('template/images/page-img/s4.jpg')}}" alt="story-img" class="rounded-circle img-fluid">
+               <div class="stories-data ml-3">
+                  <h5>Web Workshop</h5>
+                  <p class="mb-0">1 hour ago</p>
+               </div>
+            </li>
+            <li class="d-flex align-items-center">
+               <img src="{{asset('template/images/page-img/s5.jpg')}}" alt="story-img" class="rounded-circle img-fluid">
+               <div class="stories-data ml-3">
+                  <h5>Fun Events and Festivals</h5>
+                  <p class="mb-0">1 hour ago</p>
+               </div>
+            </li>
+         </ul>
+      </div>
+   </div>
+   <div class="iq-card">
+      <div class="iq-card-header d-flex justify-content-between">
+         <div class="iq-header-title">
+            <h4 class="card-title">Upcoming Birthday</h4>
+         </div>
+      </div>
+      <div class="iq-card-body">
+         <ul class="media-story m-0 p-0">
+            <li class="d-flex mb-4 align-items-center">
+               <img src="{{asset('template/images/user/01.jpg')}}" alt="story-img" class="rounded-circle img-fluid">
+               <div class="stories-data ml-3">
+                  <h5>Anna Sthesia</h5>
+                  <p class="mb-0">Today</p>
+               </div>
+            </li>
+            <li class="d-flex align-items-center">
+               <img src="{{asset('template/images/user/02.jpg')}}" alt="story-img" class="rounded-circle img-fluid">
+               <div class="stories-data ml-3">
+                  <h5>Paul Molive</h5>
+                  <p class="mb-0">Tomorrow</p>
+               </div>
+            </li>
+         </ul>
+      </div>
+   </div>
+   <div class="iq-card">
+      <div class="iq-card-header d-flex justify-content-between">
+         <div class="iq-header-title">
+            <h4 class="card-title">Suggested Pages</h4>
+         </div>
+         <div class="iq-card-header-toolbar d-flex align-items-center">
+            <div class="dropdown">
+               <span class="dropdown-toggle" id="dropdownMenuButton01" data-toggle="dropdown" aria-expanded="false" role="button">
+               <i class="ri-more-fill"></i>
+               </span>
+               <div class="dropdown-menu dropdown-menu-right" aria-labelledby="dropdownMenuButton01" style="">
+                  <a class="dropdown-item" href="#"><i class="ri-eye-fill mr-2"></i>View</a>
+                  <a class="dropdown-item" href="#"><i class="ri-delete-bin-6-fill mr-2"></i>Delete</a>
+                  <a class="dropdown-item" href="#"><i class="ri-pencil-fill mr-2"></i>Edit</a>
+                  <a class="dropdown-item" href="#"><i class="ri-printer-fill mr-2"></i>Print</a>
+                  <a class="dropdown-item" href="#"><i class="ri-file-download-fill mr-2"></i>Download</a>
+               </div>
+            </div>
+         </div>
+      </div>
+      <div class="iq-card-body">
+         <ul class="suggested-page-story m-0 p-0 list-inline">
+            <li class="mb-3">
+               <div class="d-flex align-items-center mb-3">
+                  <img src="{{asset('template/images/page-img/42.png')}}" alt="story-img" class="rounded-circle img-fluid avatar-50">
+                  <div class="stories-data ml-3">
+                     <h5>Iqonic Studio</h5>
+                     <p class="mb-0">Lorem Ipsum</p>
+                  </div>
+               </div>
+               <img src="{{asset('template/images/small/img-1.jpg')}}" class="img-fluid rounded" alt="Responsive image">
+               <div class="mt-3"><a href="#" class="btn d-block"><i class="ri-thumb-up-line mr-2"></i> Like Page</a></div>
+            </li>
+            <li class="">
+               <div class="d-flex align-items-center mb-3">
+                  <img src="{{asset('template/images/page-img/42.png')}}" alt="story-img" class="rounded-circle img-fluid avatar-50">
+                  <div class="stories-data ml-3">
+                     <h5>Cakes & Bakes </h5>
+                     <p class="mb-0">Lorem Ipsum</p>
+                  </div>
+               </div>
+               <img src="{{asset('template/images/small/img-2.jpg')}}" class="img-fluid rounded" alt="Responsive image">
+               <div class="mt-3"><a href="#" class="btn d-block"><i class="ri-thumb-up-line mr-2"></i> Like Page</a></div>
+            </li>
+         </ul>
+      </div>
+   </div>
+</div>

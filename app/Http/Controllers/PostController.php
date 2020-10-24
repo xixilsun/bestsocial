db2ad09343edf33a40a71b2cbe74211c581c563c
xixilsun <xixilliasun@gmail.com>
2020-10-24 14:14:49 +0700

    edit dikit

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -55,10 +55,10 @@ class PostController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
-    {
-        //
-    }
+    // public function show($id)
+    // {
+    //     //
+    // }
 
     /**
      * Show the form for editing the specified resource.
